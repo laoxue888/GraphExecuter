35eede1f3ec1f385b83a6b908c91365d3d70f4a4
laoxue888 <2388245387@qq.com>
2025-07-09 11:14:30 +0800

    update

---
+++ graph_executer/nodes/common/screen/clipboard.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+from PySide6.QtCore import QObject
+from NodeGraphQt import BaseNode, NodeBaseWidget
+from utils.general import find_nodes_folder
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+__all__ = ('NameNode')
+
+class NameNode(BaseNode, QObject):
+    __identifier__ = find_nodes_folder(__file__)[1]
+    NODE_NAME = 'name'
+
+    def __init__(self):
+        super(NameNode, self).__init__()
+        self.add_input('image_data')
+        self.add_output('processed_image_data')
+
+    def execute(self):
+        """节点执行函数"""
+        image_data = getattr(self.input(0).connected_ports()[0].node(), self.input(0).connected_ports()[0].name())
+
+        # 处理数据
+        processed_image_data = image_data
+
+        # 输出数据
+        setattr(self, 'processed_image_data', processed_image_data)
+
+
+    def set_messageSignal(self, messageSignal):
+        self.messageSignal = messageSignal
+
+    def close_node(self,):
+        """整个软件窗体关闭时调用"""
+
+    def _del_node(self):
+        """删除节点前调用"""
